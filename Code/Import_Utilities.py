cb2d0caf1e0304b6ba8b44a859af6a720a71a94d
Ukiah <ukiah.heasley@gmail.com>
2022-04-28 13:25:54 -0700

    Added Altair package to Utilities and added EDA

---
+++ Code/Import_Utilities.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.linear_model import LinearRegression
+import os
+import altair as alt
+from vega_datasets import data
+import matplotlib.pyplot as plt
